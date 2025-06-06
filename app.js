38fe6c76a1e92f32205023d242709088df3ac455
unknown <raudael95@gmail.com>
2025-06-06 17:44:02 -0400

    3

--- app.js
+++ app.js
@@ -465,7 +465,7 @@ const productos = [
       14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27
     ]
   },
-  {
+ /* {
     id: 101,
     nombre: "Hamburguesas Mixtas",
     precio: 2.3,
@@ -473,7 +473,7 @@ const productos = [
     description: "Bolsa de 5 Hamburguesas de 90 gr cada una", 
     categoria: "Alimentos/Cárnicos",
     municipios: [, 4, 5, 6, 7, 8, 9, 10, 11,14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
-  },
+  },*/
   // Alimentos/Líquidos
   {
     id: 17,
